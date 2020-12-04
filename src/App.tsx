e9e9e8f2d63945e4d7dde869a4b2b61313d93bc0
Thomas Z Lukoma <tzlukoma@gmail.com>
2020-12-04 16:57:15 -0500

    feat: :sparkles: Adds search by title, author and sku
    Integrate lunr for in-browser search on book state
    
    #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 import { auth } from './base'
 
 import { useAuthState } from 'react-firebase-hooks/auth'
@@ -9,6 +10,8 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from './theme'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
+import TextField from '@material-ui/core/TextField'
+import FormControl from '@material-ui/core/FormControl'
 
 import Button from '@material-ui/core/Button'
 
@@ -20,9 +23,12 @@ import BookList from './components/BookList'
 import { ReactComponent as PlusIcon } from './assets/dark-icons/plus-circle.svg'
 import SignInPage from './pages/SignInPage'
 
+
+
 const App: React.FC = () => {
   const [user] = useAuthState(auth)
   const [dialogOpen, setDialogOpen] = useState(false)
+  const [searchTerm, setSearchTerm] = useState(" ")
 
   const signedIn: boolean = !!auth.currentUser
   console.log(user)
@@ -48,23 +54,45 @@ const App: React.FC = () => {
           {(token || signedIn) ? (
             <>
               <Container style={{ padding: 20 }}>
-                <Grid
-                  item
-                  xs={12}
-                  style={{ display: 'flex', paddingBottom: 5 }}
-                >
-                  <Typography variant='h4' style={{ paddingRight: 20 }}>
-                    Book List
+                <Grid container>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={4}
+                    style={{ display: 'flex', paddingBottom: 5 }}
+                  >
+                    <Typography variant='h4' style={{ paddingRight: 20 }}>
+                      Book List
                   </Typography>
-                  <Button onClick={handleOpen}>
-                    <PlusIcon style={{ marginRight: 10 }} /> Add a book
+                    <Button onClick={handleOpen}>
+                      <PlusIcon style={{ marginRight: 10 }} /> Add a book
                   </Button>
+                  </Grid>
+                  <Grid item
+                    xs={12}
+                    sm={8}>
+                    <div style={{ marginBottom: 10 }}>
+                      <FormControl style={{ width: "80%" }}>
+                        <TextField
+                          name="searchTerm"
+                          placeholder="Search for a book by title, author or sku"
+                          size="small"
+                          variant='outlined'
+                          value={searchTerm}
+                          onChange={(e) => setSearchTerm(e.target.value)}
+
+                        />
+                      </FormControl>
+                    </div>
+
+                  </Grid>
                 </Grid>
+
                 <Grid item xs={12}>
                   <Divider />
                 </Grid>
                 <Grid item xs={12}>
-                  <BookList />
+                  <BookList searchTerm={searchTerm} />
                 </Grid>
                 <Grid container spacing={3}>
                   <Grid item xs={12}></Grid>
